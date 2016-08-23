d77002765501dacfbce05b59c22ab73c09ac2ce6
Wilko <perruchon.wilko.isart@gmail.com>
2016-08-23 20:05:31 +0200

    models done

--- Hogwart/polls/models.py
+++ Hogwart/polls/models.py
@@ -2,4 +2,38 @@ from __future__ import unicode_literals
 
 from django.db import models
 
-# Create your models here.
+class Student(models.Model):
+    student_id = models.IntegerField(default=0)
+    student_name = models.CharField(max_length=30)
+    points = models.IntegerField(default=0)
+    def __str__(self):
+        return self.student_name
+    def join_course(self, course):
+        course.append(self)
+
+class House(models.Model):
+    house_name = models.CharField(max_length=30)
+    house_students = models.ManyToManyField(Student)
+    house_points = models.IntegerField(default=0)
+    def __str__(self):
+        return self.house_name
+    def count_point():
+        house_points = 0
+        for student in house_students:
+            hourse_points += student.points
+
+class Course(models.Model):
+    course_name = models.CharField(max_length=30)
+    course_students = models.ManyToManyField(Student);
+    def __str__(self):
+        return self.course_name
+            
+class Teacher(models.Model):
+    teacher_name = models.CharField(max_length=30)
+    course_teached = models.ForeignKey(Course, on_delete=models.CASCADE)
+    def __str__(self):
+        return self.teacher_name
+    def add_points(student, points):
+        student.points += points
+    def retrieve_points(student, points):
+        student.points -= points
--- Hogwart/polls/views.py
+++ Hogwart/polls/views.py
@@ -1,3 +1,11 @@
-from django.shortcuts import render
+from django.shortcuts import get_object_or_404, render
+from django.http import HttpResponseRedirect
+from django.core.urlresolvers import reverse
+from django.views import generic
 
-# Create your views here.
+class IndexView(generic.ListView):
+    template_name = 'polls/index.html'
+    context_object_name = 'hogwarts_intra'
+
+    def get_queryset(self):
+        return "welcome"
