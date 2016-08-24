5c0ad57060f8c9f4a9cd56f0e67b68d5ec95e0c7
Wilko <perruchon.wilko.isart@gmail.com>
2016-08-24 20:12:21 +0200

    Admin fully functionnal

--- Hogwart/polls/admin.py
+++ Hogwart/polls/admin.py
@@ -1,3 +1,28 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Teacher, Course, Student, House
+
+class TeacherAdmin(admin.ModelAdmin):
+    fieldsets = [
+        (None,{'fields': ['teacher_name']}),
+        ('Course teached',{'fields':['course_teached']}),
+    ]
+class HouseAdmin(admin.ModelAdmin):
+    fieldsets = [
+        (None,{'fields': ['house_name']}),
+        ('House students',{'fields':['house_students']}),
+    ]
+class StudentAdmin(admin.ModelAdmin):
+    fieldsets = [
+        (None,{'fields': ['student_name']}),
+        ('Student points',{'fields':['points']}),
+    ]
+class CourseAdmin(admin.ModelAdmin):
+    fieldsets = [
+        (None,{'fields': ['course_name']}),
+        ('Course students',{'fields':['course_students']}),
+    ]
+admin.site.register(Teacher, TeacherAdmin)
+admin.site.register(House, HouseAdmin)
+admin.site.register(Student, StudentAdmin)
+admin.site.register(Course, CourseAdmin)
